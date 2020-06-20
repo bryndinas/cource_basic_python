049f8d80885a4c008e9a22e9b269af4d54c79362
Артем <cooler292@mail.ru>
2020-06-20 09:49:24 +0300

    выавыа

--- task_1.py
+++ task_1.py
@@ -1,1 +1,14 @@
-задание 1
+name = 'Artem'
+age = 25
+while True:
+    name = input('Введите имя \n')
+    if name.isalpha():
+        name = str (name)
+        break
+print(name)
+while True:
+    age = input('Введите возраст \n')
+    if age .isdigit():
+        age= int(age)
+        break
+print(age)
