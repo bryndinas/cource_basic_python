bb7607f4a8aba0cf3872d0b6aed0a4b28b810422
Артем <cooler292@mail.ru>
2020-06-21 13:03:44 +0300

    урок 3 задание 1

---
+++ les 3_task 1.py
@@ -0,0 +1,14 @@
+def may_func ():
+    try:
+        val_1 = float(input("Укажите первое число: "))
+        val_2 = float(input("Укажите второе число: "))
+    except ValueError:
+        return
+    else:
+        if val_1 or val_2 == 0:
+           print('На 0 делить нельзя')
+    finally:
+        res = val_1 / val_2
+    return res
+
+print(may_func())
