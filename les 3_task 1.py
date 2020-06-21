e9a64b4714aa4f0a09630c49365c66580041aa0f
Артем <cooler292@mail.ru>
2020-06-21 13:08:17 +0300

    урок 3 задание 1

--- les 3_task 1.py
+++ les 3_task 1.py
@@ -2,13 +2,11 @@ def may_func ():
     try:
         val_1 = float(input("Укажите первое число: "))
         val_2 = float(input("Укажите второе число: "))
-    except ValueError:
-        return
-    else:
-        if val_1 or val_2 == 0:
-           print('Деление на 0')
-    finally:
         res = val_1 / val_2
-    return res
+        return res
+    except ZeroDivisionError:
+        print('Деление на 0')
+    except ValueError:
+        print('Введите число')
 
 print(may_func())
