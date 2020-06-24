e80ef9f16c16be532b694951a21c1413743f2559
Артем <cooler292@mail.ru>
2020-06-24 14:36:22 +0300

    урок 3 задание 5

---
+++ les 3_task 5.py
@@ -0,0 +1,44 @@
+def my_func (str_value = input().split()):
+    print(type(str_value))
+    res = 0
+    for el in str_value:
+        el = int(el)
+        res += el
+    return res
+
+com = input()
+f = 0
+while True:
+    if com != 'q':
+        com = com.split()
+        total = my_func(com)
+        f = f + total
+        print(f'Сумма чисел {f}')
+        com = input('Введите число (остановить "q":')
+    else:
+        print(f'Финиш, сумма {f}')
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
