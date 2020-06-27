b7e0317a750162797d522676bab4fe470c5c2c84
Артем <cooler292@mail.ru>
2020-06-27 12:00:17 +0300

    урок 4 задание 1

--- les 4_task 1.py
+++ les 4_task 1.py
@@ -1,4 +1,10 @@
-stavka= int(input('Введите ставку \n'))
-our = int(input('Введите выробатку в часах \n'))
-salary = stavka * our
-print(f'Ваша заработная плата составляет {salary}')
+from sys import argv
+
+_, hour, stavka, bonus, *__ = argv
+
+try:
+    salary = float(hour)*float(stavka)+float(bonus)
+    print(f'Ваша заработная плата составляет {salary}')
+except ValueError as e:
+    print('Ошибка')
+    print(e)
