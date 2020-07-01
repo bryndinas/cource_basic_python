16e6125f1f9d39927bd8854c36331cf8ca372642
Артем <cooler292@mail.ru>
2020-07-01 15:54:38 +0300

    урок 5 задание 2

--- les 5_task 2.py
+++ les 5_task 2.py
@@ -1,9 +1,9 @@
 with open('task_2.txt','r', encoding='UTF-8') as my_file:
     value = 0
     for string in my_file:
-     print(len(string.split()))
+     print(f"Количество слов в строке: {(len(string.split()))}")
      value += 1
-print (value)
+print (f"Количество строк: {value}")
 
 
 
