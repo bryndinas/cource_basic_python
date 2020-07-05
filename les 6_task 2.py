0ea6b7347a73409db6f1a84e0dc0667d82c428b5
Артем <cooler292@mail.ru>
2020-07-05 16:54:31 +0300

    урок 6 задание 2

--- les 6_task 2.py
+++ les 6_task 2.py
@@ -1,1 +1,14 @@
-Проверка
+class Road:
+
+    def __init__(self, length, width):
+        self.__width = width
+        self.__length = length
+
+    def massa_as(self):
+        result = self.__width * self.__length * 25 * 5
+        itog = result / 1000
+
+        print(f'Для покрытия дороги нужно {itog} тонн асфальта')
+
+s = Road(12,50)
+s.massa_as()
